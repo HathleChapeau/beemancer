7aad266ea99a2db9d2eaa990930459811dd583b1
Adri <adridrodu94@gmail.com>
2026-03-11 18:35:36 +0100

    fix

--- src/main/java/com/chapeau/apica/client/model/hoverbike/SaddlePartModelC.java
+++ src/main/java/com/chapeau/apica/client/model/hoverbike/SaddlePartModelC.java
@@ -1,7 +1,7 @@
 /**
  * ============================================================
  * [SaddlePartModelC.java]
- * Description: Selle HoverBee - variante C (geometrie identique, texture differente)
+ * Description: Selle HoverBee - variante C (2 cubes lateraux relies par une barre)
  * ============================================================
  *
  * DEPENDANCES:
@@ -9,7 +9,6 @@
  * | Dependance          | Raison                | Utilisation                    |
  * |---------------------|----------------------|--------------------------------|
  * | HoverbikePartModel  | Classe parent        | Heritage modele partie         |
- * | SaddlePartModel     | Geometrie            | Reutilisation createLayerDef   |
  * | HoverbikePart       | Enum type            | SADDLE                         |
  * ------------------------------------------------------------
  *
@@ -25,12 +24,16 @@ import com.chapeau.apica.Apica;
 import com.chapeau.apica.common.entity.mount.HoverbikePart;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.phys.Vec3;
 
 /**
- * Selle variante C : meme geometrie que la variante A, texture differente.
+ * Selle variante C : 2 cubes 1x2.25x2.25 aux extremites relies par une barre centrale.
  */
 public class SaddlePartModelC extends HoverbikePartModel {
 
@@ -44,7 +47,31 @@ public class SaddlePartModelC extends HoverbikePartModel {
     }
 
     public static LayerDefinition createLayerDefinition() {
-        return SaddlePartModel.createLayerDefinition();
+        MeshDefinition mesh = new MeshDefinition();
+        PartDefinition root = mesh.getRoot();
+
+        // Cube gauche: 1x2.25x2.25, centre a -3 sur X
+        root.addOrReplaceChild("cube_left",
+                CubeListBuilder.create()
+                        .texOffs(0, 0)
+                        .addBox(-0.5F, -1.125F, -1.125F, 1.0F, 2.25F, 2.25F),
+                PartPose.offset(-3.0F, 0F, 0F));
+
+        // Cube droit: 1x2.25x2.25, centre a +3 sur X
+        root.addOrReplaceChild("cube_right",
+                CubeListBuilder.create()
+                        .texOffs(0, 0)
+                        .addBox(-0.5F, -1.125F, -1.125F, 1.0F, 2.25F, 2.25F),
+                PartPose.offset(3.0F, 0F, 0F));
+
+        // Barre de liaison: relie les deux cubes (5 de large, 2 de haut, 2 de profond)
+        root.addOrReplaceChild("connector",
+                CubeListBuilder.create()
+                        .texOffs(0, 5)
+                        .addBox(-2.5F, -1.0F, -1.0F, 5.0F, 2.0F, 2.0F),
+                PartPose.offset(0F, 0F, 0F));
+
+        return LayerDefinition.create(mesh, 16, 8);
     }
 
     @Override
