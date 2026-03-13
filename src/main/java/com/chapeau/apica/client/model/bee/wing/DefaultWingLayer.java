d54b55bfb01e3bfb3558eba3fe9192f74b00b2a7
Hath <adridrodu94@gmail.com>
2026-03-13 16:25:18 +0100

    visual

--- src/main/java/com/chapeau/apica/client/model/bee/wing/DefaultWingLayer.java
+++ src/main/java/com/chapeau/apica/client/model/bee/wing/DefaultWingLayer.java
@@ -36,16 +36,17 @@ public final class DefaultWingLayer {
                 CubeListBuilder.create(), PartPose.offset(0.0F, 19.0F, 0.0F));
 
         bone.addOrReplaceChild("right_wing",
-                CubeListBuilder.create().texOffs(2, 20)
-                        .addBox(-12.0F, 0.0F, 0.0F, 18.0F, 0.0F, 12.0F),
+                CubeListBuilder.create().texOffs(2, 18)
+                        .mirror()
+                        .addBox(-18.0F, -0.001F, 0.0F, 18.0F, 0.0F, 12.0F),
                 PartPose.offsetAndRotation(-1.5F, -4.0F, -3.0F, 0.0F, -0.2618F, 0.0F));
 
 
         bone.addOrReplaceChild("left_wing",
-                CubeListBuilder.create().texOffs(-12, 7)
-                        .addBox(0.0F, 0.0F, 0.0F, 18.0F, 0.0F, 12.0F),
+                CubeListBuilder.create().texOffs(2, 18)
+                        .addBox(0.0F, -0.001F, 0.0F, 18.0F, 0.0F, 12.0F),
                 PartPose.offsetAndRotation(1.5F, -4.0F, -3.0F, 0.0F, 0.2618F, 0.0F));
 
-        return LayerDefinition.create(mesh, 32, 32);
+        return LayerDefinition.create(mesh, 64, 32);
     }
 }
